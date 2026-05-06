8d3d4caea6e011fc03ea4e139635635f7ddd1c7f
arikdutta <duttaarik@gmail.com>
2026-05-06 17:22:05 +0200

    Add new changes

---
+++ source/basic-programming.py
@@ -0,0 +1,30 @@
+print("Hello, World!")
+print("Welcome to Python programming.")
+print("My name is ________")  # Replace with your name
+      
+print("I am learning Python.")
+print("Python is a great language for beginners.")     
+print("My name is Arik")
+print("I live in Spain")
+print("I want to become AI Engineer")           
+
+name = "Arik"
+age = 14
+city = "Australia"   
+print("Hello, my name is ", name,age,city)
+print("I am",age,"years old.")
+print("I live in",city)
+print("Hello,  name",name)
+print("You are",age,"years old.")
+
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You cannot vote yet.")
+
+age = input("Please enter your age: ")
+age = int(age)
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You cannot vote yet.")
